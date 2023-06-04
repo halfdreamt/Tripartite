10abceaf584a1d36d9f52938437760fb33c7c653
halfdreamt <31080937+halfdreamt@users.noreply.github.com>
2023-06-04 00:50:04 -0400

    Added a manageWater behavior in the physical system

--- model/system/physical_system.py
+++ model/system/physical_system.py
@@ -8,23 +8,24 @@ class physical_system:
     # perform checks and such
     def update(self):
         if self.entity.physical.alive:
-            if self.physical.life.health <= 0:
+            life = self.physical.life
+            if life.health <= 0:
                 self.die()
 
-            if self.world.isAdjacentToItem(self.physical.xcoord, self.physical.ycoord, "water"):
-                self.physical.life.thirst = 100
-            elif self.physical.life.thirst > 0:
-                self.physical.life.thirst -= 1
-
-            if self.physical.life.thirst <= 0 and self.physical.life.health > 0:
-                self.physical.life.health -= 1
-
-            # if self.physical.thirst <= 10:
-                    # TODO: add search for water logic
+            self.manageWater()
                 
-            if self.physical.life.moveVectorStack:
-                dx, dy = self.physical.life.moveVectorStack.pop()
+            if life.moveVectorStack:
+                dx, dy = life.moveVectorStack.pop()
                 self.physical.setLocation(self.physical.xcoord + dx, self.physical.ycoord + dy)
     
     def die(self):
         self.health = 0
+
+    def manageWater(self):
+        if self.world.isAdjacentToItem(self.physical.xcoord, self.physical.ycoord, "water"):
+            self.physical.thirst = 100
+        elif self.physical.life.thirst > 0:
+            self.physical.life.thirst -= 1
+
+        if self.physical.life.thirst <= 0 and self.physical.life.health > 0:
+            self.physical.life.health -= 1

29c991afca6e5afd8915f6a91c5e00e8b49fe887
halfdreamt <31080937+halfdreamt@users.noreply.github.com>
2023-07-01 20:55:37 -0400

    Corrections to adjusted tile draw function

--- view/displays/localView.py
+++ view/displays/localView.py
@@ -19,11 +19,7 @@ class LocalView:
         screen_width = self.pgdisplay.screen.get_width()
         screen_height = self.pgdisplay.screen.get_height()
 
-        # Calculate the map tile coordinates of the visible screen area
-        visible_tile_left = max(0, int(self.pgdisplay.camera_x / TILESIZE))
-        visible_tile_top = max(0, int(self.pgdisplay.camera_y / TILESIZE))
-        visible_tile_right = min(self.pgdisplay.MAPWIDTH, visible_tile_left + math.ceil(screen_width / (TILESIZE * self.pgdisplay.zoom_level)))
-        visible_tile_bottom = min(self.pgdisplay.MAPHEIGHT, visible_tile_top + math.ceil(screen_height / (TILESIZE * self.pgdisplay.zoom_level)))
+        visible_tile_left, visible_tile_top, visible_tile_right, visible_tile_bottom = self.pgdisplay.calculate_visible_tiles(screen_width, screen_height)
 
         for layer_name, tile_data in self.pgdisplay.world.map.layers.items():
             for y in range(visible_tile_top, visible_tile_bottom):
@@ -34,23 +30,21 @@ class LocalView:
                         tile_screen_x = (x * TILESIZE - self.pgdisplay.camera_x) * self.pgdisplay.zoom_level
                         tile_screen_y = (y * TILESIZE - self.pgdisplay.camera_y) * self.pgdisplay.zoom_level
 
-                        # Check if tile is within the visible screen area
-                        if tile_screen_x + TILESIZE >= 0 and tile_screen_x < screen_width and tile_screen_y + TILESIZE >= 0 and tile_screen_y < screen_height:
-                            for i in reversed(range(len(self.pgdisplay.tileset_firstgids))):
-                                if tile_id >= self.pgdisplay.tileset_firstgids[i]:
-                                    tile_id -= self.pgdisplay.tileset_firstgids[i]
-                                    tile_x, tile_y = tile_id % (self.pgdisplay.tilesets[i].get_width() // TILESIZE), tile_id // (self.pgdisplay.tilesets[i].get_width() // TILESIZE)
-                                    tile_image = self.pgdisplay.tilesets[i].subsurface(pygame.Rect(tile_x * TILESIZE, tile_y * TILESIZE, TILESIZE, TILESIZE))
-                                    tile_image = pygame.transform.scale(tile_image, (int(TILESIZE*self.pgdisplay.zoom_level), int(TILESIZE*self.pgdisplay.zoom_level)))
+                        for i in reversed(range(len(self.pgdisplay.tileset_firstgids))):
+                            if tile_id >= self.pgdisplay.tileset_firstgids[i]:
+                                tile_id -= self.pgdisplay.tileset_firstgids[i]
+                                tile_x, tile_y = tile_id % (self.pgdisplay.tilesets[i].get_width() // TILESIZE), tile_id // (self.pgdisplay.tilesets[i].get_width() // TILESIZE)
+                                tile_image = self.pgdisplay.tilesets[i].subsurface(pygame.Rect(tile_x * TILESIZE, tile_y * TILESIZE, TILESIZE, TILESIZE))
+                                tile_image = pygame.transform.scale(tile_image, (int(TILESIZE*self.pgdisplay.zoom_level), int(TILESIZE*self.pgdisplay.zoom_level)))
 
-                                    # Create a dark overlay with the calculated darkness
-                                    overlay = pygame.Surface(tile_image.get_size(), pygame.SRCALPHA)
-                                    overlay.fill((0, 0, 0, int(darkness * 200)))
+                                # Create a dark overlay with the calculated darkness
+                                overlay = pygame.Surface(tile_image.get_size(), pygame.SRCALPHA)
+                                overlay.fill((0, 0, 0, int(darkness * 200)))
 
-                                    # Apply the overlay to the tile image
-                                    tile_image.blit(overlay, (0, 0))
+                                # Apply the overlay to the tile image
+                                tile_image.blit(overlay, (0, 0))
 
-                                    self.screen.blit(tile_image, (tile_screen_x, tile_screen_y))
+                                self.screen.blit(tile_image, (tile_screen_x, tile_screen_y))
 
     def draw_local_view(self):
         self.draw_tiles()

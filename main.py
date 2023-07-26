1c3f28975bfeea1c00fef436bb6e3a2c7debf7ec
halfdreamt <31080937+halfdreamt@users.noreply.github.com>
2023-07-25 21:43:07 -0400

    Updated comments in main

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ from data.dataFactory import dataFactory
 # Initialize pygame
 pygame.init()
 
-# Initialize the settings manager with a file path to the config file (contains the file paths to the master files among other settings)
+# Initialize the settings manager with a file path to the config file and get the master file paths, display settings, and frame rate
 settings_manager = SettingsManager("rec/config.json")
 master_file_paths = settings_manager.get_master_file_paths()
 display_settings = settings_manager.get_display_settings()
+frame_rate = settings_manager.get_frame_rate()
 
-# Initialize data factory with the master file paths (inserts the master json data into the DB if needed)
+# Initialize data factory with the master file paths (inserts the master json data into the DB if needed), and get the master data and tile data
 data_factory = dataFactory(master_file_paths)
 master_data = data_factory.get_master_data()
 tile_data = data_factory.get_tile_images()
 
-# Initialize the model
+# Initialize the model, get the initial map and time
 ModelManager = modelManager(master_data)
-
 initial_map = ModelManager.get_map()
 initial_time = ModelManager.get_time()
 
-# Initialize the display
+# Initialize the view manager with the tile data, display settings, initial map, and initial time
 viewManager = PGDisplay(tile_data, display_settings, initial_map, initial_time)
 
-# Initialize the event handler
+# Initialize the event handler with references to the view manager and model manager
 controlManager = PGEvents(viewManager, ModelManager)
 
 # Initialize the clock
@@ -51,11 +51,10 @@ while not controlManager.quit:
             ModelManager.tick()
             
     # Limit the frame rate
-    clock.tick(settings_manager.get_frame_rate())
+    clock.tick(frame_rate)
 
+    # Update the display
     viewManager.set_time(ModelManager.get_time())
-
-    # Redraw screen
     viewManager.set_map(ModelManager.get_map())
 
 pygame.quit()
